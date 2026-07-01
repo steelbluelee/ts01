aae6a75ad86a37f7b40e00f7467b6688cd5ee30d
SKLee <steelblue.lee@gmail.com>
2026-07-01 16:33:13 +0900

    2026-07-01-16-33-13

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,23 @@
 const btn = document.getElementById("btn")! as HTMLButtonElement;
 const input = document.getElementById("todoinput")! as HTMLInputElement;
+const form = document.querySelector("form")!;
+const list = document.getElementById("todolist")!;
 
-btn.addEventListener("click", function () {
-  alert(input.value);
+function handleSubmit(e: SubmitEvent) {
+  e.preventDefault();
+  const newTodoText = input.value;
+  const newLI = document.createElement("LI");
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  newLI.append(newTodoText);
+  newLI.append(checkbox);
+  list.append(newLI);
   input.value = "";
-});
+}
+
+form.addEventListener("submit", handleSubmit);
+
+// btn.addEventListener("click", function () {
+//   alert(input.value);
+//   input.value = "";
+// });
