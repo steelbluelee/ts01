473f6cec859eb9d5337d2b9cafeda5d32097f2ef
SKLee <steelblue.lee@gmail.com>
2026-07-02 16:42:55 +0900

    2026-07-02-16-42-55

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+interface Todo {
+  text: string;
+  completed: boolean;
+}
+
+const todos: Todo[] = [];
+
 const btn = document.getElementById("btn")! as HTMLButtonElement;
 const input = document.getElementById("todoinput")! as HTMLInputElement;
 const form = document.querySelector("form")!;
@@ -5,14 +12,22 @@ const list = document.getElementById("todolist")!;
 
 function handleSubmit(e: SubmitEvent) {
   e.preventDefault();
-  const newTodoText = input.value;
+  const newTodo: Todo = {
+    text: input.value,
+    completed: false,
+  };
+  createTodo(newTodo);
+  todos.push(newTodo);
+  input.value = "";
+}
+
+function createTodo(todo: Todo) {
   const newLI = document.createElement("LI");
   const checkbox = document.createElement("input");
   checkbox.type = "checkbox";
-  newLI.append(newTodoText);
+  newLI.append(todo.text);
   newLI.append(checkbox);
   list.append(newLI);
-  input.value = "";
 }
 
 form.addEventListener("submit", handleSubmit);
